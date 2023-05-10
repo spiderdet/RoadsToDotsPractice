47f7248e2de01ef02a5761250f7469efb51be29c
Yunkai Zheng <yunkai.zheng@unity.cn>
2023-05-10 20:20:39 +0800

    docs: 更新注释、模板

--- Assets/EntitiesPractice/Lesson2/Scripts/Systems/CubeRotateSystemChunk.cs
+++ Assets/EntitiesPractice/Lesson2/Scripts/Systems/CubeRotateSystemChunk.cs
@@ -7,12 +7,13 @@ using Unity.Burst.Intrinsics;//v128属于该命名空间
 
 namespace DOTS.DOD.LESSON2
 {
+    [BurstCompile]
     public struct CubeRotateJobChunk : IJobChunk//不用partial因为dots不会再生成代码来补全，不像IJobEntity
     {
         [ReadOnly] public float elapsedTime;
         //IJobChunk需要额外传递句柄，就是IJobEntity中自动检索的component
-        public ComponentTypeHandle<LocalTransform> TransformTypeHandle;
-        [ReadOnly] public ComponentTypeHandle<RotateSpeed> RotateSpeedTypeHandle;
+        public ComponentTypeHandle<LocalTransform> TransformTypeHandle;//这个类型肯定是通过ComponentTypeHandle<T>来定义的，不可能直接是LocalTransformTypeHandle
+        [ReadOnly] public ComponentTypeHandle<RotateSpeed> RotateSpeedTypeHandle; //记得能只读的就只读
 
         public void Execute(in ArchetypeChunk chunk, int unfilteredChunkIndex, bool useEnabledMask,
             in v128 chunkEnabledMask)
@@ -41,6 +42,7 @@ namespace DOTS.DOD.LESSON2
     [UpdateInGroup(typeof(RotateCubeWithIJobChunkSystemGroup))]
     public partial struct CubeRotateSystemChunk : ISystem
     {
+        //private EntityQueryBuilder queryBuilder;会报错！
         private EntityQuery _rotateCubes;
         private ComponentTypeHandle<LocalTransform> _transformTypeHandle;
         private ComponentTypeHandle<RotateSpeed> _rotateSpeedTypeHandle;
@@ -48,6 +50,7 @@ namespace DOTS.DOD.LESSON2
         [BurstCompile]
         public void OnCreate(ref SystemState state)
         {
+            //如果将queryBuilder定义为system内变量会报错!! 因为EntityQueryBuilder是 ref struct。https://learn.microsoft.com/zh-cn/dotnet/csharp/language-reference/builtin-types/ref-struct
             var queryBuilder = new EntityQueryBuilder(Allocator.Temp).WithAll<RotateSpeed, LocalTransform>();
             //尽量用state来getEntityQuery和GetComponentTypeHandle，不要用EntityManager
             _rotateCubes = state.GetEntityQuery(queryBuilder);
@@ -58,7 +61,7 @@ namespace DOTS.DOD.LESSON2
         [BurstCompile]
         public void OnUpdate(ref SystemState state)
         {    
-            _transformTypeHandle.Update(ref state);
+            _transformTypeHandle.Update(ref state);//别忘了要update
             _rotateSpeedTypeHandle.Update(ref state);
             var job = new CubeRotateJobChunk
             {
@@ -66,7 +69,7 @@ namespace DOTS.DOD.LESSON2
                 TransformTypeHandle = _transformTypeHandle,
                 RotateSpeedTypeHandle = _rotateSpeedTypeHandle
             };
-            state.Dependency = job.ScheduleParallel(_rotateCubes, state.Dependency);
+            state.Dependency = job.ScheduleParallel(_rotateCubes, state.Dependency);//IJobChunk要在参数里给EntityQuery！
         }
 
         [BurstCompile]
